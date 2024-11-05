96b4e27553ad638fbd7c6126fb094d0305270c38
Natanal <djawjdgus93@naver.com>
2024-11-05 15:45:11 +0900

    Create programmers_문자열_곱하기.py

---
+++ programmers/기초/02. 연산/programmers_문자열_곱하기.py
@@ -0,0 +1,15 @@
+# 프로그래머스 문제 풀이 - 문자열 곱하기 (181940)
+
+## 문제 정보
+# **문제 이름**: 문자열 곱하기
+# **문제 번호**: 181940
+# **링크**: [링크](https://programmers.co.kr/learn/courses/30/lessons/181940)
+
+## 문제 
+# 문제 설명 :
+# 문자열 my_string과 정수 k가 주어질 때, 
+# my_string을 k번 반복한 문자열을 return 하는 solution 함수를 작성해 주세요.
+
+## 코드
+def solution(my_string, k):
+    return my_string*k
