c146525e8aa1def639aa71d4bb517b0e542460c2
Natanal <djawjdgus93@naver.com>
2024-11-05 12:42:17 +0900

    Update [1929]_소수_구하기.py

--- baekjoon/단계별/약수, 배수와 소수 2/[1929]_소수_구하기.py
+++ baekjoon/단계별/약수, 배수와 소수 2/[1929]_소수_구하기.py
@@ -18,7 +18,7 @@ arr = [0] * (N + 1)
 
 arr[1] = 1
 
-for i in range(2, N) :
+for i in range(2, int(N ** 0.5) + 1) :
     if arr[i] : continue
     for j in range(i*2, N+1, i) :
         arr[j] += 1
