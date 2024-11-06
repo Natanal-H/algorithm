d81d21c51dfce910235e33275739adc2ef3f54fe
Natanal <djawjdgus93@naver.com>
2024-11-06 16:49:53 +0900

    Create programmers_n_번째_원소부터.py

---
+++ programmers/기초/06. 리스트(배열)/programmers_n_번째_원소부터.py
@@ -0,0 +1,15 @@
+# 프로그래머스 문제 풀이 - n 번째 원소부터 (181892)
+
+## 문제 정보
+# **문제 이름**: n 번째 원소부터
+# **문제 번호**: 181892
+# **링크**: [링크](https://programmers.co.kr/learn/courses/30/lessons/181892)
+# **풀이 날짜** : 2024-11-06
+
+## 문제 
+# 문제 설명 :
+# 정수 리스트 num_list와 정수 n이 주어질 때, n 번째 원소부터 마지막 원소까지의 모든 원소를 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+
+## 코드
+def solution(num_list, n):
+    return num_list[n-1:]
