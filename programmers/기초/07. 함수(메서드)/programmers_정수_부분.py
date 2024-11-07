51fca0f51ba8016228a7b1dff9ec9692ec88fded
Natanal <djawjdgus93@naver.com>
2024-11-07 16:37:44 +0900

    Create programmers_정수_부분.py

---
+++ programmers/기초/07. 함수(메서드)/programmers_정수_부분.py
@@ -0,0 +1,15 @@
+# 프로그래머스 문제 풀이 - 정수 부분 (181850)
+
+## 문제 정보
+# **문제 이름**: 정수 부분
+# **문제 번호**: 181850
+# **링크**: [링크](https://programmers.co.kr/learn/courses/30/lessons/181850)
+# **풀이 날짜** : 2024-11-07
+
+## 문제 
+# 문제 설명 :
+# 실수 flo가 매개 변수로 주어질 때, flo의 정수 부분을 return하도록 solution 함수를 완성해주세요.
+
+## 코드
+def solution(flo):
+    return int(flo)
