8c2b6962d67f531d17bf8647a5e7e15e5a596d6f
Natanal <djawjdgus93@naver.com>
2024-11-07 09:55:16 +0900

    Create programmers_대문자로_바꾸기.py

---
+++ programmers/기초/05. 문자열/programmers_대문자로_바꾸기.py
@@ -0,0 +1,15 @@
+# 프로그래머스 문제 풀이 - 대문자로 바꾸기 (181877)
+
+## 문제 정보
+# **문제 이름**: 대문자로 바꾸기
+# **문제 번호**: 181877
+# **링크**: [링크](https://programmers.co.kr/learn/courses/30/lessons/181877)
+# **풀이 날짜** : 2024-11-07
+
+## 문제 
+# 문제 설명 :
+# 알파벳으로 이루어진 문자열 myString이 주어집니다. 모든 알파벳을 대문자로 변환하여 return 하는 solution 함수를 완성해 주세요.
+
+## 코드
+def solution(myString):
+    return myString.upper()
