30fa16cee49a6b77399fb9e6057ef5e29dec73e8
Natanal <djawjdgus93@naver.com>
2024-11-08 12:14:49 +0900

    Create programmers_각도기.py

---
+++ programmers/입문/page 2/programmers_각도기.py
@@ -0,0 +1,25 @@
+# 프로그래머스 문제 풀이 - 각도기 (120829)
+
+## 문제 정보
+# **문제 이름**: 각도기
+# **문제 번호**: 120829
+# **링크**: [링크](https://programmers.co.kr/learn/courses/30/lessons/120829)
+# **풀이 날짜** : 2024-11-08
+
+## 문제 
+# 문제 설명 :
+# 각에서 0도 초과 90도 미만은 예각, 90도는 직각, 90도 초과 180도 미만은 둔각 180도는 평각으로 분류합니다. 
+# 각 angle이 매개변수로 주어질 때 예각일 때 1, 직각일 때 2, 둔각일 때 3, 평각일 때 4를 return하도록 solution 함수를 완성해주세요.
+#
+# 예각 : 0 < angle < 90
+# 직각 : angle = 90
+# 둔각 : 90 < angle < 180
+# 평각 : angle = 180
+
+## 코드
+def solution(angle):
+    if angle < 90 : return 1
+    if angle == 90 : return 2
+    if angle < 180 : return 3
+    if angle == 180 : return 4
+    return 0
