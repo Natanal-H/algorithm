517b77b1be98e326ff12a5b9112103d8fca32699
Natanal <djawjdgus93@naver.com>
2024-11-08 11:01:37 +0900

    Create programmers_몫_구하기.py

---
+++ programmers/입문/page 1/programmers_몫_구하기.py
@@ -0,0 +1,15 @@
+# 프로그래머스 문제 풀이 - 몫 구하기 (120805)
+
+## 문제 정보
+# **문제 이름**: 몫 구하기
+# **문제 번호**: 120805
+# **링크**: [링크](https://programmers.co.kr/learn/courses/30/lessons/120805)
+# **풀이 날짜** : 2024-11-08
+
+## 문제 
+# 문제 설명 :
+# 정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+
+## 코드
+def solution(num1, num2):
+    return num1 // num2
