8094b5a8a5b1249bd7420487793df526b4c02996
Natanal <djawjdgus93@naver.com>
2024-11-08 13:06:32 +0900

    Create programmers_구슬을_나누는_경우의_수.py

---
+++ programmers/입문/page 2/programmers_구슬을_나누는_경우의_수.py
@@ -0,0 +1,29 @@
+# 프로그래머스 문제 풀이 - 구슬을 나누는 경우의 수 (120840)
+
+## 문제 정보
+# **문제 이름**: 구슬을 나누는 경우의 수
+# **문제 번호**: 120840
+# **링크**: [링크](https://programmers.co.kr/learn/courses/30/lessons/120840)
+# **풀이 날짜** : 2024-11-08
+
+## 문제 
+# 문제 설명 :
+# 머쓱이는 구슬을 친구들에게 나누어주려고 합니다. 구슬은 모두 다르게 생겼습니다. 
+# 머쓱이가 갖고 있는 구슬의 개수 balls와 친구들에게 나누어 줄 구슬 개수 share이 매개변수로 주어질 때, 
+# balls개의 구슬 중 share개의 구슬을 고르는 가능한 모든 경우의 수를 return 하는 solution 함수를 완성해주세요.
+
+## 코드
+def solution(balls, share):
+    a = b = 1
+    
+    for i in range(share) :
+        a *= balls - i
+        b *= i + 1
+        
+    return a // b
+
+## 다른 코드
+import math
+
+def solution(balls, share):
+    return math.comb(balls, share)
