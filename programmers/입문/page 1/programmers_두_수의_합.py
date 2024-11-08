2a149c7a9e9fb4d40c50956e6e7f2e7286359d7d
Natanal <djawjdgus93@naver.com>
2024-11-08 10:57:49 +0900

    Create programmers_두_수의_합.py

---
+++ programmers/입문/page 1/programmers_두_수의_합.py
@@ -0,0 +1,15 @@
+# 프로그래머스 문제 풀이 - 두 수의 합 (120802)
+
+## 문제 정보
+# **문제 이름**: 두 수의 합
+# **문제 번호**: 120802
+# **링크**: [링크](https://programmers.co.kr/learn/courses/30/lessons/120802)
+# **풀이 날짜** : 2024-11-08
+
+## 문제 
+# 문제 설명 :
+# 정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
+
+## 코드
+def solution(num1, num2):
+    return num1 + num2
