ea51aecbfbe0494dce474026a6bada45370813d5
Natanal <djawjdgus93@naver.com>
2024-11-08 10:59:42 +0900

    Create programmers_두_수의_차.py

---
+++ programmers/입문/page 1/programmers_두_수의_차.py
@@ -0,0 +1,15 @@
+# 프로그래머스 문제 풀이 - 두 수의 차 (120803)
+
+## 문제 정보
+# **문제 이름**: 두 수의 차
+# **문제 번호**: 120803
+# **링크**: [링크](https://programmers.co.kr/learn/courses/30/lessons/120803)
+# **풀이 날짜** : 2024-11-08
+
+## 문제 
+# 문제 설명 :
+# 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+
+## 코드
+def solution(num1, num2):
+    return num1 - num2
