a8021e24dd024ac08b99b818bf1cf45ba199c0ab
Natanal <djawjdgus93@naver.com>
2024-11-08 11:13:15 +0900

    Create programmers_나머지_구하기.py

---
+++ programmers/입문/page 1/programmers_나머지_구하기.py
@@ -0,0 +1,15 @@
+# 프로그래머스 문제 풀이 - 나머지 구하기 (120810)
+
+## 문제 정보
+# **문제 이름**: 나머지 구하기
+# **문제 번호**: 120810
+# **링크**: [링크](https://programmers.co.kr/learn/courses/30/lessons/120810)
+# **풀이 날짜** : 2024-11-08
+
+## 문제 
+# 문제 설명 :
+# 정수 num1, num2가 매개변수로 주어질 때, num1를 num2로 나눈 나머지를 return 하도록 solution 함수를 완성해주세요.
+
+## 코드
+def solution(num1, num2):
+    return num1 % num2
