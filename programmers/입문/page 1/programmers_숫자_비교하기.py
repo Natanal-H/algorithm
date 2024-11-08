613a8a4d0f4c0ea3eb31ef2c1db24f5db2181e0e
Natanal <djawjdgus93@naver.com>
2024-11-08 11:04:09 +0900

    Create programmers_숫자_비교하기.py

---
+++ programmers/입문/page 1/programmers_숫자_비교하기.py
@@ -0,0 +1,15 @@
+# 프로그래머스 문제 풀이 - 숫자 비교하기 (120807)
+
+## 문제 정보
+# **문제 이름**: 숫자 비교하기
+# **문제 번호**: 120807
+# **링크**: [링크](https://programmers.co.kr/learn/courses/30/lessons/120807)
+# **풀이 날짜** : 2024-11-08
+
+## 문제 
+# 문제 설명 :
+# 정수 num1과 num2가 매개변수로 주어집니다. 두 수가 같으면 1 다르면 -1을 retrun하도록 solution 함수를 완성해주세요.
+
+## 코드
+def solution(num1, num2):
+    return 1 if num1 == num2 else -1
