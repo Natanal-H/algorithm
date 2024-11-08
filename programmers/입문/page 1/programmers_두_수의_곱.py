acaea6e80f594e8c966a299041dfb1a52b4f6580
Natanal <djawjdgus93@naver.com>
2024-11-08 11:00:38 +0900

    Create programmers_두_수의_곱.py

---
+++ programmers/입문/page 1/programmers_두_수의_곱.py
@@ -0,0 +1,15 @@
+# 프로그래머스 문제 풀이 - 두 수의 곱 (120804)
+
+## 문제 정보
+# **문제 이름**: 두 수의 곱
+# **문제 번호**: 120804
+# **링크**: [링크](https://programmers.co.kr/learn/courses/30/lessons/120804)
+# **풀이 날짜** : 2024-11-08
+
+## 문제 
+# 문제 설명 :
+# 정수 num1, num2가 매개변수 주어집니다. num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
+
+## 코드
+def solution(num1, num2):
+    return num1 * num2
