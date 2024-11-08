e15f67dcc328ef8853ceadcace00833ad1ed026e
Natanal <djawjdgus93@naver.com>
2024-11-08 10:41:30 +0900

    Create programmers_중복된_숫자_개수.py

---
+++ programmers/입문/page 1/programmers_중복된_숫자_개수.py
@@ -0,0 +1,15 @@
+# 프로그래머스 문제 풀이 - 중복된 숫자 개수 (120583)
+
+## 문제 정보
+# **문제 이름**: 중복된 숫자 개수
+# **문제 번호**: 120583
+# **링크**: [링크](https://programmers.co.kr/learn/courses/30/lessons/120583)
+# **풀이 날짜** : 2024-11-08
+
+## 문제 
+# 문제 설명 :
+# 정수가 담긴 배열 array와 정수 n이 매개변수로 주어질 때, array에 n이 몇 개 있는 지를 return 하도록 solution 함수를 완성해보세요.
+
+## 코드
+def solution(array, n):
+    return array.count(n)
