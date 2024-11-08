bb3947cb2946beabedc915eb220934a24e13abc9
Natanal <djawjdgus93@naver.com>
2024-11-08 11:57:05 +0900

    Create programmers_배열_뒤집기.py

---
+++ programmers/입문/page 2/programmers_배열_뒤집기.py
@@ -0,0 +1,15 @@
+# 프로그래머스 문제 풀이 - 배열 뒤집기 (120821)
+
+## 문제 정보
+# **문제 이름**: 배열 뒤집기
+# **문제 번호**: 120821
+# **링크**: [링크](https://programmers.co.kr/learn/courses/30/lessons/120821)
+# **풀이 날짜** : 2024-11-08
+
+## 문제 
+# 문제 설명 :
+# 정수가 들어 있는 배열 num_list가 매개변수로 주어집니다. num_list의 원소의 순서를 거꾸로 뒤집은 배열을 return하도록 solution 함수를 완성해주세요.
+
+## 코드
+def solution(num_list):
+    return num_list[::-1]
