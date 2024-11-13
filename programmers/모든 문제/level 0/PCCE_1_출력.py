f6aba80cc5b45190c80b895daf9616e29fc6bc23
Natanal <djawjdgus93@naver.com>
2024-11-13 11:08:54 +0900

    Create PCCE_1_출력.py

---
+++ programmers/모든 문제/level 0/PCCE_1_출력.py
@@ -0,0 +1,21 @@
+# 프로그래머스 문제 풀이 - [PCCE 기출문제] 1번 / 출력 (250133)
+
+## 문제 정보
+# **문제 이름**: [PCCE 기출문제] 1번 / 출력
+# **문제 번호**: 250133
+# **문제 레벨**: 0
+# **링크**: [링크](https://programmers.co.kr/learn/courses/30/lessons/250133)
+# **풀이 날짜** : 2024-11-13
+
+## 문제 
+# 문제 설명 :
+# 주어진 초기 코드는 변수에 데이터를 저장하고 출력하는 코드입니다. 아래와 같이 출력되도록 빈칸을 채워 코드를 완성해 주세요.
+
+## 코드
+string_msg = 'Spring is beginning'
+int_val = 3
+string_val = '3'
+
+print(string_msg)
+print(int_val + 10)
+print(string_val + "10")
